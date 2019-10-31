6f94623e53d6832abe3ced9ded386e4826f9f421
BUHLER Geoffray <geoffray.buhler@gmail.com>
2019-10-31 16:25:48 +0100

    js graph en cours

---
+++ js/graph.js
@@ -0,0 +1,30 @@
+var canvas = document.querySelector('canvas');
+
+var ctx = canvas.getContext('2d');
+
+var x = [];
+
+//modifier les valeur apres pissétou
+
+const canvasSize = {
+    x: document.body.scrollHeight,
+    y: document.body.scrollWidth
+};
+
+//a mettre au moment du enter en mode graph
+
+makeABoard = function () {
+    for (let i = -10; i <= 10; i++) {
+       x.push (i);
+    };
+};
+
+drawGraph (inp){
+    tabl.forEach(x => {
+        x 
+    });
+}
+
+// updateAff (){
+
+// }
