c9b9ccf5beb111f17a65ad43d9abe480532976ba
ricou <e.lanza@free.fr>
2019-11-04 17:41:26 +0100

    ajout code dans keyDetect.js

--- js/keyDetect.js
+++ js/keyDetect.js
@@ -9,49 +9,26 @@ function test(evt) {
                 inputKeyPressed(evt.key);
         }
 
-        // backspace
-        if ((evt.key == "Backspace")) {
-                alert('backspace');
+        // "x"
+        if ((evt.keyCode == 120)) {
+                // code
         }
-      
+
+        // "(" and ")"
+        if ((evt.keyCode == 40) || (evt.keyCode == 41)) {
+                inputKeyPressed(evt.key);
+        }
+
+        // backspace
+
+
+        // Del
+
         // Enter
         if ((evt.keyCode == 13)) {
-               //calculate();
+                //calculate();
         }
-}
 
 
-
-function key_detect_calc(id, evt) {
-        alert('test');
-        if ((evt.keyCode > 95) && (evt.keyCode < 106)) {
-                var nbr = evt.keyCode - 96;
-                add_calc(id, nbr);
-        } else if ((evt.keyCode > 47) && (evt.keyCode < 58)) {
-                var nbr = evt.keyCode - 48;
-                add_calc(id, nbr);
-        } else if (evt.keyCode == 107) {
-                f_calc(id, '+');
-        } else if (evt.keyCode == 109) {
-                f_calc(id, '-');
-        } else if (evt.keyCode == 106) {
-                f_calc(id, '*');
-        } else if (evt.keyCode == 111) {
-                f_calc(id, '');
-        } else if (evt.keyCode == 110) {
-                add_calc(id, '.');
-        } else if (evt.keyCode == 190) {
-                add_calc(id, '.');
-        } else if (evt.keyCode == 188) {
-                add_calc(id, '.');
-        } else if (evt.keyCode == 13) {
-                f_calc(id, '=');
-        } else if (evt.keyCode == 46) {
-                f_calc(id, 'ce');
-        } else if (evt.keyCode == 8) {
-                f_calc(id, 'nbs');
-        } else if (evt.keyCode == 27) {
-                f_calc(id, 'ce');
-        }
-        return true;
+        alert(evt.key);
 }
