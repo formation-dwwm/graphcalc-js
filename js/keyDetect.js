793706f45cb6098bacc920ef84c20bd224a86b1b
amaraabdallah <amara.abdalla@gmail.com>
2019-11-04 16:19:06 +0100

    new keypress

---
+++ js/keyDetect.js
@@ -0,0 +1,66 @@
+function test(evt){
+    
+        alert(evt);
+}
+
+
+
+function key_detect_calc(id,evt)
+{
+    alert('test');
+        if((evt.keyCode>95) && (evt.keyCode<106))
+        {
+                var nbr = evt.keyCode-96;
+                add_calc(id,nbr);
+        }
+        else if((evt.keyCode>47) && (evt.keyCode<58))
+        {
+                var nbr = evt.keyCode-48;
+                add_calc(id,nbr);
+        }
+        else if(evt.keyCode==107)
+        {
+                f_calc(id,'+');
+        }
+        else if(evt.keyCode==109)
+        {
+                f_calc(id,'-');
+        }
+        else if(evt.keyCode==106)
+        {
+                f_calc(id,'*');
+        }
+        else if(evt.keyCode==111)
+        {
+                f_calc(id,'');
+        }
+        else if(evt.keyCode==110)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==190)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==188)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==13)
+        {
+                f_calc(id,'=');
+        }
+        else if(evt.keyCode==46)
+        {
+                f_calc(id,'ce');
+        }
+        else if(evt.keyCode==8)
+        {
+                f_calc(id,'nbs');
+        }
+        else if(evt.keyCode==27)
+        {
+                f_calc(id,'ce');
+        }
+        return true;
+}
