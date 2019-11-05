e8dd6666d6674e141a88aaf4b1a62105053b9d7b
BUHLER Geoffray <geoffray.buhler@gmail.com>
2019-11-05 14:31:38 +0100

    fix du lien DoYouWantThis

--- js/DoYouWantThis.js
+++ js/DoYouWantThis.js
@@ -3,8 +3,6 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    showInstallPromotion();
 });
 
 btnAdd.addEventListener('click', (e) => {
