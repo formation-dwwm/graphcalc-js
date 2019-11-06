6c13bbc28cd8c77375ca8cbe50c79a4712cc7e44
83010-07-03 <83010-07-03@exchange.ad.afpanet>
2019-11-06 16:56:57 +0100

    Fix graph orientation. Begin fixing strange behaviors...

--- js/graph.js
+++ js/graph.js
@@ -2,9 +2,9 @@ var xValues = [];
 
 var screenGraph = "y= ";
 
-var zoom = {
-    x: 5,
-    y: 5
+var scale = {
+    x: 20,
+    y: 20
 }
 
 var canvas = document.querySelector('canvas');
@@ -13,18 +13,21 @@ var ctx = canvas.getContext('2d');
 //modifier les valeur apres pissétou
 
 const canvasSize = {
-    x: canvas.clientHeight,
-    y: canvas.clientWidth
+    x: canvas.clientWidth,
+    y: canvas.clientHeight
 };
+const startX = -2,
+      endX = 10,
+      stepX = 0.1;
 
-    var midx = canvasSize.x /2 ;
-    var midy = canvasSize.y /2 ;
+var midX = canvasSize.x / 2;
+var midY = canvasSize.y / 2;
 
-udpateCanvasSize = function(){
-    canvasSize.x = canvas.clientHeight;
-    canvasSize.y = canvas.clientWidth;
-    midx = canvasSize.x /2 ;
-    midy = canvasSize.y /2 ;
+udpateCanvasSize = function () {
+    canvasSize.x = canvas.clientWidth;
+    canvasSize.y = canvas.clientHeight;
+    midX = canvasSize.x / 2;
+    midY = canvasSize.y / 2;
     makeAxes();
 }
 
@@ -32,7 +35,8 @@ udpateCanvasSize = function(){
 
 makeABoard = function () {
     udpateCanvasSize();
-    for (let i = -20; i <= 20; i++) {
+    xValues = [];
+    for (let i = startX; i <= endX; i+=stepX) {
         xValues.push(i);
     };
     clearCanvas();
@@ -40,33 +44,82 @@ makeABoard = function () {
 };
 
 makeAxes = function () {
-    ctx.strokeStyle  = "#000000";
+    ctx.strokeStyle = "#000000";
     ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.moveTo(0, midx);
-    ctx.lineTo(canvasSize.y, midx);
-    ctx.moveTo(midy, 0);
-    ctx.lineTo(midy, canvasSize.x);
+    ctx.moveTo(0, midY);
+    ctx.lineTo(canvasSize.x, midY);
+    ctx.moveTo(midX, 0);
+    ctx.lineTo(midX, canvasSize.y);
     ctx.stroke();
 };
 // = press run this function
 
 drawGraph = function () {
-    ctx.beginPath();
-    ctx.strokeStyle  = "#ff0000";
+
+    var boolInfinity= 0;
+
+    ctx.strokeStyle = "#ff0000";
     ctx.lineWidth = 1;
-    xValues.forEach(function (x) {
-        var evalResult = eval(userInput);
-        var posx = x * zoom.x; 
-        var posy = evalResult * zoom.y;
-        posy = posy * -zoom.y;
-        if (x === -10) {
-            ctx.moveTo(midy + posy, posx + midx);
-        } else {
-            ctx.lineTo(midy + posy, posx + midx);
+
+    const points = [];
+
+    // parcours des valeurs x
+    for (i=0; i<xValues.length; i++) {
+        var x= xValues[i];
+
+        //Please correct this...
+        if(Math.abs(x - 1e-5) < 0){
+            x = 0;
         }
-    });
-    ctx.stroke();
+        
+        // evalue la fonction y=
+        var evalResult = eval(userInput);
+        
+        // recalcul / echelle
+        var posx = x * scale.x;
+        var posy = evalResult * scale.y * (-1);
+
+        const finalX = posx + midX;
+        const finalY = posy + midY;
+
+        points.push({
+            x,
+            y: evalResult,
+            finalX,
+            finalY
+        });
+    }
+    console.table(points);
+
+    for (i=0; i<points.length; i++) {
+        const point = points[i];
+        // if (evalResult===Infinity) {
+        // test sur Number.MAX_SAFE_INTEGER pour lever issue de tracé intempestif
+        // ex: fonction y=1/x
+        if(Math.abs(points.finalY) > Number.MAX_SAFE_INTEGER){
+            // tracé intermédiaire
+            // booleen pour nouveau beginPath
+            ctx.stroke();
+            boolInfinity= 1;
+            // nouvelle itération de la boucle
+            continue;
+        }
+        // début de portion de courbe
+        if ((point.x===startX) || (boolInfinity)){ 
+            ctx.beginPath();
+            ctx.moveTo(point.finalX, point.finalY); 
+            boolInfinity= 0;
+        }
+        else if (! boolInfinity){
+            ctx.lineTo(point.finalX, point.finalY);
+        }
+        
+        // tracé final
+        if (i==(points.length-1)) {
+            ctx.stroke();
+        }
+    }
 };
 
 // drawAxes = function () {
@@ -88,7 +141,7 @@ function clearCanvas() {
     ctx.strokeText(" ", 200, 290);
     makeAxes();
 
-    updateScreen();
+   // updateScreen();
     // $divInput = document.querySelector(".input-view");
     // $divInput.textContent = screenGraph + userInput;
 };
